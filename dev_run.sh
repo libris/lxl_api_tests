9e69b6818340c8c151b2fd041c7491b901085588
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2017-05-19 15:38:13 +0200

    Revert "Add a line break."
    This reverts commit 9df9c52d7b036d71162074791d09a4d62a47cd71.

--- dev_run.sh
+++ dev_run.sh
@@ -11,8 +11,7 @@ export LXLTESTING_AUTH_URL="https://libris-dev.kb.se/login/authorize"
 export LXLTESTING_LXL_LOGIN_URL="https://libris-dev.kb.se/login"
 export LXLTESTING_ROOT_URL="https://libris-dev.kb.se"
 export LXLTESTING_LOGIN_URL="https://bibdb-stg.libris.kb.se/login"
-export LXLTESTING_USERNAME="test"
-export LXLTESTING_PASSWORD="test"
+export LXLTESTING_USERNAME="test" LXLTESTING_PASSWORD="test"
 pytest -s
 
 # Run OAIPMH tests
